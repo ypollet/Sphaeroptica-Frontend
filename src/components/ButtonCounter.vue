2f29ab3acfeddb7e69734cd0f5a3b97eae75c696
Yann Pollet <pollet.yann1999@gmail.com>
2024-02-27 09:30:13 +0100

    fix merge conflicts

--- src/components/ButtonCounter.vue
+++ src/components/ButtonCounter.vue
@@ -1,11 +1,17 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { useStorage } from '@vueuse/core'
 
-const count = ref(0)
+const count = useStorage("count", 0, sessionStorage)
+
+function increment(){
+  console.log("ZELE")
+  count.value++
+};
+
 </script>
 
 <template>
-  <button @click="count++">
+  <button @click="increment()">
     You clicked me {{ count }} times.
   </button>
 </template>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,6 +5,7 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import ButtonCounter from './ButtonCounter.vue'
 </script>
 
 <template>
@@ -17,6 +18,9 @@ import SupportIcon from './icons/IconSupport.vue'
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
     provides you with all information you need to get started.
+  </WelcomeItem>
+  <WelcomeItem>
+    <ButtonCounter></ButtonCounter>
   </WelcomeItem>
 
   <WelcomeItem>
@@ -32,9 +36,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
     you need to test your components and web pages, check out
     <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
+    <a href="https://on.cypress.io/component" target="_blank" rel="noopener">Cypress Component Testing</a>.
 
     <br />
 
@@ -66,9 +68,8 @@ import SupportIcon from './icons/IconSupport.vue'
     Got stuck? Ask your question on
     <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
     Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
+    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener">StackOverflow</a>. You
+    should also subscribe to
     <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
     the official
     <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
